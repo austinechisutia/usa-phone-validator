0dbc5bb35e34c55c5e376abf712b0a752d6fb424
austinechisutia <austineakhonya624@gmail.com>
2025-04-11 19:53:32 +0300

    done

--- script.js
+++ script.js
@@ -3,11 +3,31 @@ const checkBtn = document.getElementById("check-btn")
 const clearBtn = document.getElementById("clear-btn")
 const result = document.getElementById("results-div");
 
+
+
 checkBtn.addEventListener("click", function(){
   const inputValue = userInput.value.trim();
 
   if(inputValue===""){
-    result.textContent = "Please provide a phone number"
+    alert("Please provide a phone number")
     return;
   }
+
+  const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s.-]?)\d{3}([\s.-]?)\d{4}$/;
+
+
+ if(phoneRegex.test(inputValue)){
+   result.textContent = `Valid US number: ${inputValue}`
+ }else{
+    result.textContent = `Invalid US number: ${inputValue}`
+ }
 })
+
+
+
+
+clearBtn.addEventListener("click", function(){
+  result.textContent = ""
+})
+
+
